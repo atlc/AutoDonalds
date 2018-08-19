9f129e9f11a01e9a1f1248c45a220ff0436c1141
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-19 13:30:12 -0500

    Completes survey and randomly assigns a few restaurants that have allegedly been eaten at

--- content.js
+++ content.js
@@ -1,11 +1,29 @@
 $('document').ready(function() {
     let nextButton = document.getElementById('NextButton');
-    let atTheRestaurant = document.getElementById('R000150.1');
+    let orderLocation = document.getElementById('R000150.1'); // At the restaurant
     let orderType = document.getElementById('R004000.3'); // Carry-out
     let overallSatisfaction = document.getElementById('R001000.5'); // Highly satisfied
     let didYouExperienceAProblem = document.getElementById('R016000.2');
     let commentBox = document.getElementById('S081000'); // Textarea asking for highly satisfied reasoning
     let didYouCustomizeYourSandwichOrWrap = document.getElementById('R000103.2'); // No
+    let mainItemOrdered = document.getElementById('R000225.99'); // "Other"
+    let breakfastItemOrdered = document.getElementById('R000166'); // "Other", follows `mainItemOrdered`
+    let didYouOrderFromDollarMenu = document.getElementById('R000329.2'); // No
+    let howWouldYouRateThisMcDonalds = document.getElementById('R000034.5'); // Much better than competitors in area
+    let whichDoYouConsiderYourFavorite = document.getElementById('R059000.7'); // McDonalds
+
+    let competitorsVisitedInPastThirtyDays = [
+        document.getElementById('R058010'), // Arbys 
+        document.getElementById('R000037'), // Dunkin Doughnuts
+        document.getElementById('R058130'), // Taco Bell
+        document.getElementById('R058120'), // Subway
+        document.getElementById('R000039'), // Chik-fil-a
+        document.getElementById('R058140'), // Wendys
+        document.getElementById('R000038'), // Starbucks       
+        document.getElementById('R058060'), // KFC
+        document.getElementById('R058020'), // Burger King
+        document.getElementById('R058160')  // "Other"       
+    ];
 
     let highlySatisfieds = [
         document.getElementById('R013000.9'),
@@ -29,22 +47,27 @@ $('document').ready(function() {
         document.getElementById('R019000.5')
     ];
 
-    if (atTheRestaurant != null) {
-        atTheRestaurant.checked = true;
+    let visitDetails = [
+        document.getElementById('R060000.1'), // In a party just by yourself
+        document.getElementById('R020000.4')
+    ];
+
+    if (orderLocation != null) {
+        orderLocation.click();
         setTimeout(() => {
             nextButton.click();
         }, 1000);
     }
 
     if (orderType != null) {
-        orderType.checked = true;
+        orderType.click();
         setTimeout(() => {
             nextButton.click();
         }, 1000);
     }
 
     if (overallSatisfaction != null) {
-        overallSatisfaction.checked = true;
+        overallSatisfaction.click();
         setTimeout(() => {
             nextButton.click();
         }, 1000);
@@ -69,7 +92,7 @@ $('document').ready(function() {
     }
 
     if(didYouExperienceAProblem != null) {
-        didYouExperienceAProblem.checked = true;
+        didYouExperienceAProblem.click();
         setTimeout(() => {
             nextButton.click();
         }, 1000);
@@ -95,5 +118,62 @@ $('document').ready(function() {
         setTimeout(() => {
             nextButton.click();
         }, 1250);
+    }
+
+    if (mainItemOrdered != null) {
+        mainItemOrdered.click();
+        setTimeout(() => {
+            nextButton.click();
+        }, 1250);
+    }
+
+    if (breakfastItemOrdered != null) {
+        breakfastItemOrdered.click();
+        setTimeout(() => {
+            nextButton.click();
+        }, 1250);
+    }
+
+    if (didYouOrderFromDollarMenu != null) {
+        didYouOrderFromDollarMenu.click();
+        setTimeout(() => {
+            nextButton.click();
+        }, 1250);
+    }
+
+    if (visitDetails[0] != null) {
+        visitDetails.forEach(function(detail) {
+            detail.click();
+        });
+        setTimeout(() => {
+            nextButton.click();
+        }, 1000);
+    }
+
+    if (howWouldYouRateThisMcDonalds != null) {
+        howWouldYouRateThisMcDonalds.click();
+        setTimeout(() => {
+            nextButton.click();
+        }, 1000);
+    }
+
+    // Randomly "eat" at between 1 and 5 competitors in past month
+    if (competitorsVisitedInPastThirtyDays[0] != null) {
+        let numberOfRestaurantsVisited = Math.floor(Math.random() * 5) + 1;
+        for (i = 0; i <= numberOfRestaurantsVisited; i++) {
+            let rand = (Math.floor(Math.random() * competitorsVisitedInPastThirtyDays.length) + 1);
+            competitorsVisitedInPastThirtyDays[rand].click();
+            delete competitorsVisitedInPastThirtyDays[rand];
+        }
+        setTimeout(() => {
+            nextButton.click();
+        }, 1000);
+    }
+
+    if (whichDoYouConsiderYourFavorite != null) {
+        whichDoYouConsiderYourFavorite.click();
+        setTimeout(() => {
+            nextButton.click();
+        }, 1000);
     }
 });

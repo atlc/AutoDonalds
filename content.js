7ee329c033d4694d8f5a2ab0f0b856b4349d5f21
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-19 14:41:56 -0500

    Implemented some error handling and added additional survey possibilities

--- content.js
+++ content.js
@@ -11,6 +11,7 @@ $('document').ready(function() {
     let didYouOrderFromDollarMenu = document.getElementById('R000329.2'); // No
     let howWouldYouRateThisMcDonalds = document.getElementById('R000034.5'); // Much better than competitors in area
     let whichDoYouConsiderYourFavorite = document.getElementById('R059000.7'); // McDonalds
+    let mcPickMenu = document.getElementById('R000279.2');
 
     let competitorsVisitedInPastThirtyDays = [
         document.getElementById('R058010'), // Arbys 
@@ -26,19 +27,23 @@ $('document').ready(function() {
     ];
 
     let highlySatisfieds = [
-        document.getElementById('R013000.9'),
-        document.getElementById('R011000.5'),
-        document.getElementById('R007000.5'),
-        document.getElementById('R008000.5'),
-        document.getElementById('R028000.5'),
-        document.getElementById('R009000.5')        
+        document.getElementById('R013000.9'), // Restroom Cleanliness NA
+        document.getElementById('R074000.5'), // Exterior cleanliness
+        document.getElementById('R009000.5'), // Friendliness of crew
+        document.getElementById('R028000.5'), // Quality of food
+        document.getElementById('R005000.5'), // Taste of food
+        document.getElementById('R008000.5'), // Speed of service
+        document.getElementById('R011000.5'), // Ease of placing order
+        document.getElementById('R000285.5'), // French fries
+        document.getElementById('R073000.5')  // Interior cleanliness
     ];
 
     let highlySatisfiedsPartTwo = [
+        document.getElementById('R007000.5'),
         document.getElementById('R073000.5'),
+        document.getElementById('R000285.5'),
+        document.getElementById('R011000.5'),
         document.getElementById('R006000.5'),
-        document.getElementById('R005000.5'),
-        document.getElementById('R074000.5'),
         document.getElementById('R015000.5')
     ];
 
@@ -56,46 +61,56 @@ $('document').ready(function() {
         orderLocation.click();
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (orderType != null) {
         orderType.click();
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (overallSatisfaction != null) {
         overallSatisfaction.click();
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (highlySatisfieds[0] != null) {
-        highlySatisfieds.forEach(function(highlySasisfied) {
-            highlySasisfied.click();
+        highlySatisfieds.forEach(function(highlySatisfied) {
+            try {
+                highlySatisfied.click();
+            } 
+            catch(error) {
+                console.error(error);
+            }
         });
         setTimeout(() => {
             nextButton.click();
-        }, 1750);
+        }, 1250);
     }
 
     if (highlySatisfiedsPartTwo[0] != null) {
         highlySatisfiedsPartTwo.forEach(function(highlySasisfied) {
-            highlySasisfied.click();
+            try {
+                highlySasisfied.click();
+            }
+            catch(error) {
+                console.error(error);
+            }
         });
         setTimeout(() => {
             nextButton.click();
-        }, 1750);
+        }, 1250);
     }
 
     if(didYouExperienceAProblem != null) {
         didYouExperienceAProblem.click();
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (likelihoods[0] != null) {
@@ -104,13 +119,13 @@ $('document').ready(function() {
         });
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (commentBox != null) {
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (didYouCustomizeYourSandwichOrWrap != null) {
@@ -147,33 +162,40 @@ $('document').ready(function() {
         });
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (howWouldYouRateThisMcDonalds != null) {
         howWouldYouRateThisMcDonalds.click();
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     // Randomly "eat" at between 1 and 5 competitors in past month
     if (competitorsVisitedInPastThirtyDays[0] != null) {
         let numberOfRestaurantsVisited = Math.floor(Math.random() * 5) + 1;
-        for (i = 0; i <= numberOfRestaurantsVisited; i++) {
+        for (i = 0; i < numberOfRestaurantsVisited; i++) {
             let rand = (Math.floor(Math.random() * competitorsVisitedInPastThirtyDays.length) + 1);
             competitorsVisitedInPastThirtyDays[rand].click();
             delete competitorsVisitedInPastThirtyDays[rand];
         }
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
     }
 
     if (whichDoYouConsiderYourFavorite != null) {
         whichDoYouConsiderYourFavorite.click();
         setTimeout(() => {
             nextButton.click();
-        }, 1000);
+        }, 1250);
+    }
+
+    if (mcPickMenu != null) {
+        mcPickMenu.click();
+        setTimeout(() => {
+            nextButton.click();
+        }, 1250);
     }
 });
